14e15ea2097a328fed6fb00f6534403b75c89a28
RyoOhashi99 <ryo.ohashi0809.git@gmail.com>
2025-02-14 11:17:10 +0900

    完成しました

---
+++ src/main/java/com/example/demo/SpringTraining4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTraining4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTraining4Application.class, args);
+	}
+
+}
