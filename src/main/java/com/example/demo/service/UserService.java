1946f5a253260c8c2b9db020b148bd690559c3c9
RyoOhashi99 <ryo.ohashi0809.git@gmail.com>
2025-02-14 15:54:43 +0900

    完成しました２

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -17,32 +17,32 @@ import com.example.demo.repository.UserRepository;
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class UserService {
-  /**
-   * ユーザー情報 Repository
-   */
-  @Autowired
-  private UserRepository userRepository;
+	/**
+	 * ユーザー情報 Repository
+	 */
+	@Autowired
+	private UserRepository userRepository;
 
-  /**
-   * ユーザー情報 全検索
-   * @return 検索結果
-   */
-  public List<User> searchAll() {
-    return userRepository.findAll();
-  }
+	/**
+	 * ユーザー情報 全検索
+	 * @return 検索結果
+	 */
+	public List<User> searchAll() {
+		return userRepository.findAll();
+	}
 
-  /**
-   * ユーザー情報 新規登録
-   * @param user ユーザー情報
-   */
-  public void create(UserRequest userRequest) {
-    Date now = new Date();
-    User user = new User();
-    user.setName(userRequest.getName());
-    user.setAddress(userRequest.getAddress());
-    user.setPhone(userRequest.getPhone());
-    user.setCreateDate(now);
-    user.setUpdateDate(now);
-    userRepository.save(user);
-  }
+	/**
+	 * ユーザー情報 新規登録
+	 * @param user ユーザー情報
+	 */
+	public void create(UserRequest userRequest) {
+		Date now = new Date();
+		User user = new User();
+		user.setName(userRequest.getName());
+		user.setAddress(userRequest.getAddress());
+		user.setPhone(userRequest.getPhone());
+		user.setCreateDate(now);
+		user.setUpdateDate(now);
+		userRepository.save(user);
+	}
 }
